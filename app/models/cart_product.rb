ec9bc5edb57c819bba5224ce8e654e1de9ebac13
Adam Hernandez <adam@digimedia.com>
2011-05-26 21:35:23 -0500

    fixed?

--- app/models/cart_product.rb
+++ app/models/cart_product.rb
@@ -4,8 +4,17 @@ class CartProduct < ActiveRecord::Base
   
   belongs_to :cart
   
+  # BEFORE:
   # cart = Cart.last || Cart.create
   # cart_product = cart.cart_products.first || CartProduct.create(:cart_id => cart.id)
+  # cart.attributes = {:id => cart.id, :cart_products_attributes => { cart_product.id => {:id => cart_product.id, :cart_id => cart.id, :price => 10.money}}}
+  # cart.save
+  # cart.cart_products.first.price
+  
+  # AFTER:
+  # cart = Cart.last || Cart.create
+  # cart_product = cart.cart_products.first || CartProduct.create(:cart_id => cart.id)
+  # cart.cart_products = [cart_product]
   # cart.attributes = {:id => cart.id, :cart_products_attributes => { cart_product.id => {:id => cart_product.id, :cart_id => cart.id, :price => 10.money}}}
   # cart.save
   # cart.cart_products.first.price
